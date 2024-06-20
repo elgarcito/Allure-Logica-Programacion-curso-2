c3ab2f7d4e85207d614a947996771bda78763556
elgaricito <edgarjaguirre@hotmail.com>
2024-06-20 20:06:45 -0300

    Final advance and conclusion of the course

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 alert('Hola mundo');
-let secretNumber= Math.floor((Math.random()*10)+1);
+//Changing the number that we multiply the Math.random() we increase or decrease the chances
+let maxNumber=10;
+let secretNumber= Math.floor((Math.random()*maxNumber)+1);
 let userNumber=0;
 let tries=0;
 const maxTries=3;
 console.log(secretNumber);
 
 while (secretNumber!=userNumber) {
-    userNumber = parseInt(prompt("Me indicas un numero por favor"));
+    userNumber = parseInt(prompt(`Me indicas un numero por favor entre 1 y ${maxNumber} por favor`));
     console.log(userNumber);
     /*
     This code make the comparison */
