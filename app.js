a83e98931ece4b527dc41ffdecd5b38c8a93efe3
elgaricito <edgarjaguirre@hotmail.com>
2024-06-20 19:16:00 -0300

    General course advance

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 alert('Hola mundo');
 let secretNumber= 6;
-let userNumber = prompt("Me indicas un numero por favor");
-console.log(userNumber);
+let userNumber=0;
+let counter=0;
 
-/*
-This code make the comparison 
-*/
-
-if (userNumber==secretNumber) {
-    //true condition
-    alert(`You guess the number this is ${secretNumber}`);
-}else {
-    //false condition
-    alert('Sorry wrong number');
-};
+while (secretNumber!=userNumber) {
+    userNumber = prompt("Me indicas un numero por favor");
+    console.log(userNumber);
+    /*
+    This code make the comparison */
+    if (userNumber==secretNumber) {
+        //true condition
+        alert(`You guess the number this is ${secretNumber} the number of tries were: ${counter}`);
+    }else if(userNumber>secretNumber){
+        //false condition
+        alert('the secret number is lower');
+        counter+=1;
+    }else if (userNumber<secretNumber){
+        alert("The secret number is higher");
+        counter+=1;
+    };
+}
