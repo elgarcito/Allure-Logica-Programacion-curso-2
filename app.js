ce7d1aa2916b85eff6847de09f4950432b239781
elgaricito <edgarjaguirre@hotmail.com>
2024-06-20 18:22:53 -0300

    First commit downloaded files from page

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Hola mundo');
+let secretNumber= 6;
+let userNumber = prompt("Me indicas un numero por favor");
+console.log(userNumber);
+
+if (userNumber==secretNumber) {
+    alert('You guess the number');
+}
