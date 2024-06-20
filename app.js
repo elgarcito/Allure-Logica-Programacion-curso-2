c7d4fb41c8293dee3bd386cfadbb92913058f0fb
elgaricito <edgarjaguirre@hotmail.com>
2024-06-20 18:43:07 -0300

    Move folders

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let secretNumber= 6;
 let userNumber = prompt("Me indicas un numero por favor");
 console.log(userNumber);
 
+/*
+This code make the comparison 
+*/
+
 if (userNumber==secretNumber) {
-    alert('You guess the number');
-}
+    //true condition
+    alert(`You guess the number this is ${secretNumber}`);
+}else {
+    //false condition
+    alert('Sorry wrong number');
+};
