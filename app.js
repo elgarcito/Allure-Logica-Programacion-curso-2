6997d38afd6b4a9cf396c3e3ee74a8778278c15a
elgaricito <edgarjaguirre@hotmail.com>
2024-06-20 20:01:31 -0300

    Class advance

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 alert('Hola mundo');
-let secretNumber= 6;
+let secretNumber= Math.floor((Math.random()*10)+1);
 let userNumber=0;
-let counter=0;
+let tries=0;
+const maxTries=3;
+console.log(secretNumber);
 
 while (secretNumber!=userNumber) {
-    userNumber = prompt("Me indicas un numero por favor");
+    userNumber = parseInt(prompt("Me indicas un numero por favor"));
     console.log(userNumber);
     /*
     This code make the comparison */
-    if (userNumber==secretNumber) {
+    if (userNumber===secretNumber) {
         //true condition
-        alert(`You guess the number this is ${secretNumber} the number of tries were: ${counter}`);
+        alert(`You guess the number this is ${secretNumber} you use ${tries} ${tries==1 ? 'try':"tries"} `);
     }else if(userNumber>secretNumber){
         //false condition
         alert('the secret number is lower');
-        counter+=1;
+        tries++;
     }else if (userNumber<secretNumber){
         alert("The secret number is higher");
-        counter+=1;
+        tries++;
     };
+    if (tries>=maxTries){
+        alert(`Game over number max of tries ${maxTries}`);
+        break;
+    }
 }
